ae86ff80d963c95354bcb3b353588ed7198ebabe
_eranbraun <eranb@carbyne911.com>
2021-09-01 14:52:29 +0300

    added core file

--- ci/entrypoint.sh
+++ ci/entrypoint.sh
@@ -136,7 +136,7 @@ function configure_application() {
     SGW_CREDS=$(get_conf_entry "sgw_creds")
     SGW_CREDS="${SGW_CREDS%\"}"
     SGW_CREDS_WITHOUT_QUOTES="${SGW_CREDS#\"}"
-    NEW_LINE6='rtsp_url="rtsp://'${SGW_CREDS_WITHOUT_QUOTES}''$SGW_URL':1935/'$SGW_APPLICATION'/" 
+    NEW_LINE6='rtsp_url="rtsp://'${SGW_CREDS_WITHOUT_QUOTES}''$SGW_URL':1935/'$SGW_APPLICATION'/" '
     sed -i '/rtsp_url=/c\'"$NEW_LINE6" "${FILE2}"
 
 }
--- ci/scripts/is_health.sh
+++ ci/scripts/is_health.sh
@@ -68,6 +68,7 @@ function health {
             if [ "$JANUS_APP_IS_RUNNING_STATUS" == "0" ]; then
                   echo "Janus app is not running!" >>$LOG_FILE
                   cd /home/ubuntu/WGWService
+                  touch core
                   gdb -q -n -ex bt -batch janus core >>$LOG_FILE 2>&1
                   "$FILEPATH" second "$FILEPATH" &
                   exit
