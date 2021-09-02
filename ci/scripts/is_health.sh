a5a727e708af1c2c98777886716e14f634d0627b
_eranbraun <eranb@carbyne911.com>
2021-09-02 17:18:40 +0300

    removed legacy logic with script arguments

--- ci/scripts/is_health.sh
+++ ci/scripts/is_health.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-RUN=$1
 JANUS_WEB_SOCKET_SIGNALLING_PORT=443
 JANUS_HTTP_ADMIN_PORT=7889
 
@@ -52,7 +51,6 @@ function second() {
 }
 
 function health() {
-      echo "$RUN"
 
       echo "start of health function"
 
@@ -81,38 +79,36 @@ function health() {
       JANUS_AUDIO_MIXER_THREADS_NUMBER=$(ps -To pcpu,tid,comm -C janus | sort -r -k1 | grep gstAudioMixer | wc -l)
       echo "JANUS_AUDIO_MIXER_THREADS_NUMBER:"$JANUS_AUDIO_MIXER_THREADS_NUMBER >>$LOG_FILE
 
-      if [ "$RUN" == "first" ]; then
-            JANUS_APP_IS_RUNNING_STATUS=$(ps aux | pgrep janus | wc -l)
-            JANUS_WEB_SOCKET_SIGNALLING_PORT_STATUS=$(
-                  nc -zv localhost ${JANUS_WEB_SOCKET_SIGNALLING_PORT} &>/dev/null
-                  echo $?
-            )
-            JANUS_HTTP_ADMIN_PORT_STATUS=$(
-                  nc -zv localhost ${JANUS_HTTP_ADMIN_PORT} &>/dev/null
-                  echo $?
-            )
+      JANUS_APP_IS_RUNNING_STATUS=$(ps aux | pgrep janus | wc -l)
+      JANUS_WEB_SOCKET_SIGNALLING_PORT_STATUS=$(
+            nc -zv localhost ${JANUS_WEB_SOCKET_SIGNALLING_PORT} &>/dev/null
+            echo $?
+      )
+      JANUS_HTTP_ADMIN_PORT_STATUS=$(
+            nc -zv localhost ${JANUS_HTTP_ADMIN_PORT} &>/dev/null
+            echo $?
+      )
 
-            echo "RUN:-----1---- "
-            if [ "$JANUS_APP_IS_RUNNING_STATUS" == "0" ]; then
-                  echo "Janus app is not running!" >>$LOG_FILE
-                  cd /home/ubuntu/WGWService
-                  gdb -q -n -ex bt -batch janus core >>$LOG_FILE 2>&1
-                  second
-                  exit
-            elif [ "$JANUS_WEB_SOCKET_SIGNALLING_PORT_STATUS" == "$PORT_IS_CLOSED" ]; then
-                  echo "Janus port=${JANUS_WEB_SOCKET_SIGNALLING_PORT} is not listening!" >>$LOG_FILE_PATH
-                  second
-                  exit
-            elif [ "$JANUS_HTTP_ADMIN_PORT_STATUS" == "$PORT_IS_CLOSED" ]; then
-                  echo "Janus port=${JANUS_HTTP_ADMIN_PORT} is not listening!" >>$LOG_FILE
-                  second
-                  exit
-            else
-                  echo "First Test..PASS"
-                  exit
-            fi
-
+      echo "RUN:-----1---- "
+      if [ "$JANUS_APP_IS_RUNNING_STATUS" == "0" ]; then
+            echo "Janus app is not running!" >>$LOG_FILE
+            cd /home/ubuntu/WGWService
+            gdb -q -n -ex bt -batch janus core >>$LOG_FILE 2>&1
+            second
+            exit
+      elif [ "$JANUS_WEB_SOCKET_SIGNALLING_PORT_STATUS" == "$PORT_IS_CLOSED" ]; then
+            echo "Janus port=${JANUS_WEB_SOCKET_SIGNALLING_PORT} is not listening!" >>$LOG_FILE_PATH
+            second
+            exit
+      elif [ "$JANUS_HTTP_ADMIN_PORT_STATUS" == "$PORT_IS_CLOSED" ]; then
+            echo "Janus port=${JANUS_HTTP_ADMIN_PORT} is not listening!" >>$LOG_FILE
+            second
+            exit
+      else
+            echo "First Test..PASS"
+            exit
       fi
+
 }
 echo "$(date) with user $(whoami) running health script"
 health
