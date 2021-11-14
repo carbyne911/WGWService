231739425506be2232d21694c179a0e65076ce01
eran-braun <87475345+eran-braun@users.noreply.github.com>
2021-11-15 01:01:15 +0200

    udpated deadlock sim

--- plugins/janus_videoroom.c
+++ plugins/janus_videoroom.c
@@ -7584,8 +7584,28 @@ static void janus_videoroom_recorder_close(janus_videoroom_publisher *participan
 		janus_recorder_destroy(rc);
 	}
 }
-void *function1();
-void *function2();
+void *function1(){
+	  pthread_mutex_lock(&first_mutex);  // to acquire the resource/mutex lock
+     printf("Thread ONE acquired first_mutex\n");
+     sleep(1);
+     pthread_mutex_lock(&second_mutex);
+     printf("Thread ONE acquired second_mutex\n");
+     pthread_mutex_unlock(&second_mutex); // to release the resource
+     printf("Thread ONE released second_mutex\n");
+     pthread_mutex_unlock(&first_mutex);
+     printf("Thread ONE released first_mutex\n");
+}
+void *function2(){
+	pthread_mutex_lock(&second_mutex);
+     printf("Thread TWO acquired second_mutex\n");
+     sleep(1);
+     pthread_mutex_lock(&first_mutex);
+     printf("Thread TWO acquired first_mutex\n");
+     pthread_mutex_unlock(&first_mutex);
+     printf("Thread TWO released first_mutex\n");
+     pthread_mutex_unlock(&second_mutex);
+     printf("Thread TWO released second_mutex\n");
+}
 pthread_mutex_t first_mutex; // mutex lock
 pthread_mutex_t second_mutex;
 
